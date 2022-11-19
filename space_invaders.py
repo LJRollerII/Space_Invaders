77f7c2250c96e5a848a10368aa960431ca8396eb
LJRollerII <ljcoolkid@optonline.net>
2022-11-19 00:28:58 -0500

    Successfully uploaded images

---
+++ space_invaders.py
@@ -0,0 +1,21 @@
+import pygame
+import os
+import time
+import random
+
+# Load Images
+RED_SPACESHIP = pygame.image.load(os.path.join("assets", "pixel_ship_red_small.png"))
+GREEN_SPACESHIP = pygame.image.load(os.path.join("assets", "pixel_ship_green_small.png"))
+BLUE_SPACESHIP = pygame.image.load(os.path.join("assets", "pixel_ship_blue_small.png"))
+
+# Player Ship
+YELLOW_SPACESHIP = pygame.image.load(os.path.join("assets", "pixel_ship_yellow.png"))
+
+# Load Lasers
+RED_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_red.png"))
+GREEN_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_green.png"))
+BLUE_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_blue.png"))
+YELLOW_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"))
+
+# Load Background Image
+BG = pygame.image.load(os.path.join("assets", "background-black.png"))
